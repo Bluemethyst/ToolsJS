a5b371e48446509b2e6e88a8eb4eefc434fc9a54
Bluemethyst <bluemethyst@gmail.com>
2025-10-15 14:07:44 +1300

    Begun upgrade, waiting on ExDeorum to be updated.

--- src/main/kotlin/dev/bluemethyst/mods/toolsjs/kubejs/ToolsJSPlugin.kt
+++ src/main/kotlin/dev/bluemethyst/mods/toolsjs/kubejs/ToolsJSPlugin.kt
@@ -6,6 +6,7 @@ import dev.bluemethyst.mods.toolsjs.kubejs.builders.exdeorum.*
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin
 import dev.latvian.mods.kubejs.registry.BuilderTypeRegistry
 import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
 import net.neoforged.fml.ModList
 
 @Suppress("SpellCheckingInspection")
@@ -17,19 +18,19 @@ class ToolsJSPlugin : KubeJSPlugin {
     override fun registerBuilderTypes(registry: BuilderTypeRegistry?) {
         registry!!.of(Registries.ITEM) { reg ->
             if (ModList.get().isLoaded("exdeorum")) {
-                reg.add("exdeorum:hammer", HammerItemBuilder::class.java, ::HammerItemBuilder)
-                reg.add("exdeorum:crook", CrookItemBuilder::class.java, ::CrookItemBuilder)
-                reg.add("exdeorum:watering_can", WateringCanItemBuilder::class.java, ::WateringCanItemBuilder)
-                reg.add("exdeorum:mesh", MeshItemBuilder::class.java, ::MeshItemBuilder)
+                reg.add(ResourceLocation.parse("exdeorum:hammer"), HammerItemBuilder::class.java, ::HammerItemBuilder)
+                reg.add(ResourceLocation.parse("exdeorum:crook"), CrookItemBuilder::class.java, ::CrookItemBuilder)
+                reg.add(ResourceLocation.parse("exdeorum:watering_can"), WateringCanItemBuilder::class.java, ::WateringCanItemBuilder)
+                reg.add(ResourceLocation.parse("exdeorum:mesh"), MeshItemBuilder::class.java, ::MeshItemBuilder)
             }
-            reg.add("paxel", PaxelItemBuilder::class.java, ::PaxelItemBuilder)
-            reg.add("horse_armor", HorseArmorBuilder::class.java, ::HorseArmorBuilder)
-            reg.add("shield", ShieldItemBuilder::class.java, ::ShieldItemBuilder)
-            reg.add("brush", BrushItemBuilder::class.java, ::BrushItemBuilder)
+            reg.add(ResourceLocation.parse("paxel"), PaxelItemBuilder::class.java, ::PaxelItemBuilder)
+            reg.add(ResourceLocation.parse("horse_armor"), HorseArmorBuilder::class.java, ::HorseArmorBuilder)
+            reg.add(ResourceLocation.parse("shield"), ShieldItemBuilder::class.java, ::ShieldItemBuilder)
+            reg.add(ResourceLocation.parse("brush"), BrushItemBuilder::class.java, ::BrushItemBuilder)
 
-            reg.add("fishing_rod", FishingRodItemBuilder::class.java, ::FishingRodItemBuilder)
-            reg.add("bow", BowItemBuilder::class.java, ::BowItemBuilder)
-            reg.add("crossbow", CrossbowItemBuilder::class.java, ::CrossbowItemBuilder)
+            reg.add(ResourceLocation.parse("fishing_rod"), FishingRodItemBuilder::class.java, ::FishingRodItemBuilder)
+            reg.add(ResourceLocation.parse("bow"), BowItemBuilder::class.java, ::BowItemBuilder)
+            reg.add(ResourceLocation.parse("crossbow"), CrossbowItemBuilder::class.java, ::CrossbowItemBuilder)
         }
     }
 }
